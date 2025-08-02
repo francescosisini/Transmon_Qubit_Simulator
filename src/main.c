1b47bbff7a2b8cdd3df8d98ac9a159862ea0676f
scuola sisini <francescomichelesisini@gmail.com>
2025-08-02 15:33:48 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 // main.c
 #include "resonator.h"
+#include "transmon.h"
 
 int main() {
-    // Valori realistici: L = 10 nH, C = 100 fF, Q = 10000
-    double L = 10e-9;     // 10 nanoHenry
-    double C = 100e-15;   // 100 femtoFarad
+    // --- Risonatore ---
+    double L = 10e-9;     // 10 nH
+    double C_r = 100e-15; // 100 fF
     double Q = 10000.0;
 
-    Resonator* r = crea_risonatore_LC(L, C, Q);
+    Resonator* r = crea_risonatore_LC(L, C_r, Q);
     if (!r) {
         fprintf(stderr, "Errore nella creazione del risonatore.\n");
         return 1;
@@ -15,6 +16,21 @@ int main() {
 
     stampa_risonatore(r);
 
+    // --- Transmon ---
+    double C_q = 80e-15;     // 80 fF
+    double EJ = 20e-24;      // 20 aJoule (≈ 125 GHz * hbar)
+
+    Transmon* t = crea_transmon(C_q, EJ);
+    if (!t) {
+        fprintf(stderr, "Errore nella creazione del transmon.\n");
+        free(r);
+        return 1;
+    }
+
+    stampa_transmon(t);
+
+    // --- Cleanup ---
     free(r);
+    free(t);
     return 0;
 }
