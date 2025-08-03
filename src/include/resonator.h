b9a1578bf07c0b743f260e815f0f1ddb5a4f9bf5
scuola sisini <francescomichelesisini@gmail.com>
2025-08-03 18:42:32 +0200

    Update resonator.h

--- src/include/resonator.h
+++ src/include/resonator.h
@@ -3,14 +3,24 @@
 #define RESONATOR_H
 
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Costanti fisiche
 #define PI 3.141592653589793
 #define HBAR 1.0545718e-34
 #define E_CHARGE 1.60217662e-19
 
+// Nodo elettrico
+typedef struct {
+    int id;
+    const char* nome;
+} NodoElettrico;
+
 // Struttura dati per il risonatore
 typedef struct {
+    NodoElettrico* nodo_A;  // Nodo iniziale
+    NodoElettrico* nodo_B;  // Nodo finale
     double L;         // Induttanza in Henry
     double C;         // Capacita in Farad
     double omega_r;   // Frequenza angolare in rad/s
@@ -20,7 +30,7 @@ typedef struct {
 } Resonator;
 
 // Funzione per creare un risonatore LC
-Resonator* crea_risonatore_LC(double L, double C, double Q);
+Resonator* crea_risonatore_LC(double L, double C, double Q, NodoElettrico* nodo_A, NodoElettrico* nodo_B);
 
 // Funzione per stampare i parametri del risonatore
 void stampa_risonatore(const Resonator* r);
