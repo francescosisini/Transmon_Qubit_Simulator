253e705b5ed8e3d04e2140cbf7a5afb365011356
scuola sisini <francescomichelesisini@gmail.com>
2025-08-03 22:02:08 +0200

    Update circuito.h

--- src/include/circuito.h
+++ src/include/circuito.h
@@ -5,34 +5,28 @@
 #include "nodo.h"
 #include "resonator.h"
 #include "transmon.h"
-
-// Placeholder future components
-// #include "capacitor.h"
-// #include "coaxial.h"
+#include "capacitor.h"
+#include "coaxial.h"
 
 typedef struct {
     NodoElettrico** nodi;
     int num_nodi;
-
     Resonator** risonatori;
     int num_risonatori;
-
     Transmon** transmon;
     int num_transmon;
-
-    // Future additions
-    // Capacitor** condensatori;
-    // int num_condensatori;
-
-    // CoaxialCable** cavi;
-    // int num_cavi;
-
+    Capacitor** condensatori;
+    int num_condensatori;
+    CoaxialCable** coassiali;
+    int num_coassiali;
 } CircuitoElettronico;
 
 CircuitoElettronico* crea_circuito();
 void aggiungi_nodo(CircuitoElettronico* c, NodoElettrico* n);
 void aggiungi_risonatore(CircuitoElettronico* c, Resonator* r);
 void aggiungi_transmon(CircuitoElettronico* c, Transmon* t);
+void aggiungi_capacitor(CircuitoElettronico* c, Capacitor* cap);
+void aggiungi_coassiale(CircuitoElettronico* c, CoaxialCable* cavo);
 void stampa_topologia(const CircuitoElettronico* c);
 void libera_circuito(CircuitoElettronico* c);
 
