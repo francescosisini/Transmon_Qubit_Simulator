f18b000227a270b9fe0d6a68ad70ac50124aa034
scuola sisini <francescomichelesisini@gmail.com>
2025-08-04 16:09:57 +0200

    Update circuito.h

--- src/include/circuito.h
+++ src/include/circuito.h
@@ -7,26 +7,46 @@
 #include "transmon.h"
 #include "capacitor.h"
 #include "coaxial.h"
+#include "resistor.h"
+#include "generator.h"
 
 typedef struct {
     NodoElettrico** nodi;
     int num_nodi;
+
     Resonator** risonatori;
     int num_risonatori;
+
     Transmon** transmon;
     int num_transmon;
+
     Capacitor** condensatori;
     int num_condensatori;
+
     CoaxialCable** coassiali;
     int num_coassiali;
+
+    // Aggiunte necessarie:
+    Resistor** resistors;
+    int num_resistors;
+
+    Generatore** generatori;
+    int num_generatori;
 } CircuitoElettronico;
 
+// Metodi esistenti
 CircuitoElettronico* crea_circuito();
 void aggiungi_nodo(CircuitoElettronico* c, NodoElettrico* n);
 void aggiungi_risonatore(CircuitoElettronico* c, Resonator* r);
 void aggiungi_transmon(CircuitoElettronico* c, Transmon* t);
 void aggiungi_capacitor(CircuitoElettronico* c, Capacitor* cap);
 void aggiungi_coassiale(CircuitoElettronico* c, CoaxialCable* cavo);
+
+// Metodi da aggiungere
+void aggiungi_resistor(CircuitoElettronico* c, Resistor* res);
+void aggiungi_generatore(CircuitoElettronico* c, Generatore* gen);
+
+// Utility
 void stampa_topologia(const CircuitoElettronico* c);
 void libera_circuito(CircuitoElettronico* c);
 
