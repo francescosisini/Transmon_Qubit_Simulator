a0a6692053a1842b09280ab1f44beb4bcb90d168
scuola sisini <francescomichelesisini@gmail.com>
2025-08-04 18:28:11 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,34 +1,21 @@
-// main.c – Esempio base con circuito resistivo e generatore DC
-#include <stdio.h>
-#include "circuito.h"
-#include "resistor.h"
-#include "generator.h"
-#include "analisi_dc.h"
-#include "nodo.h"
-
 int main() {
-    // Creazione circuito
     CircuitoElettronico* circuito = crea_circuito();
 
-    // Creazione nodi
-    NodoElettrico* massa = crea_nodo(-1);
-    NodoElettrico* nodo1 = crea_nodo(0);
+    // Massa è nodo 0
+    NodoElettrico* massa = crea_nodo(0);
+    NodoElettrico* nodo1 = crea_nodo(1);
 
     aggiungi_nodo(circuito, massa);
     aggiungi_nodo(circuito, nodo1);
 
-    // Generatore DC 5V tra nodo1 e massa
     Generatore* gen = crea_generatore("V1", GENERATORE_DC, 5.0, 0.0, nodo1, massa);
     aggiungi_generatore(circuito, gen);
 
-    // Resistenza da 1000 ohm tra nodo1 e massa
     Resistor* res = crea_resistor("R1", 1000.0, nodo1, massa);
     aggiungi_resistor(circuito, res);
 
-    // Analisi in DC
     risolvi_dc(circuito);
 
-    // Output
     printf("Tensione nodo1: %.2f V\n", get_tensione_nodo(nodo1));
     printf("Corrente nella resistenza: %.6f A\n", get_corrente_resistenza(res));
 
