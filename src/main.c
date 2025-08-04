2e2527ade12ad13b1db5f346e7c5a49004288610
scuola sisini <francescomichelesisini@gmail.com>
2025-08-04 15:32:45 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,40 +1,32 @@
 // main.c
 #include <stdio.h>
-#include <stdlib.h>
-#include "nodo.h"
-#include "resonator.h"
-#include "transmon.h"
 #include "circuito.h"
+#include "resistor.h"
+#include "generator.h"
+#include "analisi_dc.h"
 
 int main() {
-    // Crea il circuito
-    CircuitoElettronico* circuito = crea_circuito();
+    CircuitoElettronico circuito = crea_circuito();
 
-    // Crea e registra i nodi
-    NodoElettrico* nodo1 = malloc(sizeof(NodoElettrico)); nodo1->id = 1; nodo1->nome = "A";
-    NodoElettrico* nodo2 = malloc(sizeof(NodoElettrico)); nodo2->id = 2; nodo2->nome = "B";
-    NodoElettrico* nodo3 = malloc(sizeof(NodoElettrico)); nodo3->id = 3; nodo3->nome = "C";
-    NodoElettrico* nodo4 = malloc(sizeof(NodoElettrico)); nodo4->id = 4; nodo4->nome = "D";
+    // Nodo 0 = massa, Nodo 1 = nodo positivo del generatore e della resistenza
+    NodoElettrico* massa = aggiungi_nodo(&circuito); // id 0
+    NodoElettrico* nodo1 = aggiungi_nodo(&circuito); // id 1
 
-    aggiungi_nodo(circuito, nodo1);
-    aggiungi_nodo(circuito, nodo2);
-    aggiungi_nodo(circuito, nodo3);
-    aggiungi_nodo(circuito, nodo4);
+    // Generatore DC da 5V: da massa (negativo) a nodo1 (positivo)
+    Generatore* gen = crea_generatore(GENERATORE_DC, 5.0, nodo1, massa);
+    aggiungi_generatore(&circuito, gen);
 
-    // Crea e registra un risonatore LC
-    Resonator* r = crea_risonatore_LC(1e-9, 1e-12, 1000.0, nodo1, nodo2);
-    aggiungi_risonatore(circuito, r);
+    // Resistenza da 1k Ohm tra nodo1 e massa
+    Resistor* res = crea_resistor(1000.0, nodo1, massa);
+    aggiungi_resistor(&circuito, res);
 
-    // Crea e registra un transmon
-    Transmon* t = crea_transmon(1e-12, 1e-22, nodo3, nodo4);
-    aggiungi_transmon(circuito, t);
+    // Analisi DC
+    risolvi_dc(&circuito);
 
-    // Stampa la topologia
-    stampa_topologia(circuito);
-
-    // Libera la memoria
-    libera_circuito(circuito);
-    free(nodo1); free(nodo2); free(nodo3); free(nodo4);
+    // Stampa risultati
+    printf("Tensione nodo 0 (massa): %.2f V\n", get_tensione_nodo(massa));
+    printf("Tensione nodo 1: %.2f V\n", get_tensione_nodo(nodo1));
+    printf("Corrente nella resistenza: %.6f A\n", get_corrente_resistenza(res));
 
     return 0;
 }
