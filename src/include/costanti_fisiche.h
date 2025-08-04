1a7461c301bfc5c08e57f0bfd9b5b818da121563
scuola sisini <francescomichelesisini@gmail.com>
2025-08-04 17:10:17 +0200

    Create costanti_fisiche.h

---
+++ src/include/costanti_fisiche.h
@@ -0,0 +1,11 @@
+// costanti_fisiche.h
+#ifndef COSTANTI_FISICHE_H
+#define COSTANTI_FISICHE_H
+
+// Carica elementare (Coulomb)
+#define E_CHARGE 1.602176634e-19
+
+// Costante di Planck ridotta (Joule·secondo)
+#define HBAR 1.054571817e-34
+
+#endif
