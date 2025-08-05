16c7eddc7af5a7fbfd2ed26dc843ea0cd38138cc
scuola sisini <francescomichelesisini@gmail.com>
2025-08-05 10:03:27 +0200

    Update circuito.h

--- src/include/circuito.h
+++ src/include/circuito.h
@@ -1,53 +1,46 @@
-// circuito.h
 #ifndef CIRCUITO_H
 #define CIRCUITO_H
 
+#include "resistor.h"
+#include "generator.h"
+#include "capacitor.h"
 #include "nodo.h"
 #include "resonator.h"
 #include "transmon.h"
-#include "capacitor.h"
 #include "coaxial.h"
-#include "resistor.h"
-#include "generator.h"
+
+#define MAX_COMPONENTI 100
 
 typedef struct {
-    NodoElettrico** nodi;
+    NodoElettrico* nodi[MAX_COMPONENTI];
     int num_nodi;
 
-    Resonator** risonatori;
-    int num_risonatori;
-
-    Transmon** transmon;
-    int num_transmon;
-
-    Capacitor** condensatori;
-    int num_condensatori;
-
-    CoaxialCable** coassiali;
-    int num_coassiali;
-
-    // Aggiunte necessarie:
-    Resistor** resistors;
+    Resistor* resistors[MAX_COMPONENTI];
     int num_resistors;
 
-    Generatore** generatori;
+    Generatore* generatori[MAX_COMPONENTI];
     int num_generatori;
+
+    Capacitor* capacitors[MAX_COMPONENTI];
+    int num_capacitors;
+
+    Resonator* risonatori[MAX_COMPONENTI];
+    int num_risonatori;
+
+    Transmon* transmon[MAX_COMPONENTI];
+    int num_transmon;
+
+    CoaxialCable* coassiali[MAX_COMPONENTI];
+    int num_coassiali;
 } CircuitoElettronico;
 
-// Metodi esistenti
 CircuitoElettronico* crea_circuito();
 void aggiungi_nodo(CircuitoElettronico* c, NodoElettrico* n);
+void aggiungi_resistor(CircuitoElettronico* c, Resistor* r);
+void aggiungi_generatore(CircuitoElettronico* c, Generatore* g);
+void aggiungi_capacitor(CircuitoElettronico* c, Capacitor* cap);
 void aggiungi_risonatore(CircuitoElettronico* c, Resonator* r);
 void aggiungi_transmon(CircuitoElettronico* c, Transmon* t);
-void aggiungi_capacitor(CircuitoElettronico* c, Capacitor* cap);
 void aggiungi_coassiale(CircuitoElettronico* c, CoaxialCable* cavo);
-
-// Metodi da aggiungere
-void aggiungi_resistor(CircuitoElettronico* c, Resistor* res);
-void aggiungi_generatore(CircuitoElettronico* c, Generatore* gen);
-
-// Utility
-void stampa_topologia(const CircuitoElettronico* c);
-void libera_circuito(CircuitoElettronico* c);
 
 #endif
