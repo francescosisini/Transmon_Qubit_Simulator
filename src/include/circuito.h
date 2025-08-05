db86923cb6a1a2a18c43a016339e07610e98be4b
scuola sisini <francescomichelesisini@gmail.com>
2025-08-05 10:26:23 +0200

    Update circuito.h

--- src/include/circuito.h
+++ src/include/circuito.h
@@ -9,28 +9,26 @@
 #include "transmon.h"
 #include "coaxial.h"
 
-#define MAX_COMPONENTI 100
-
 typedef struct {
-    NodoElettrico* nodi[MAX_COMPONENTI];
+    NodoElettrico** nodi;
     int num_nodi;
 
-    Resistor* resistors[MAX_COMPONENTI];
+    Resistor** resistors;
     int num_resistors;
 
-    Generatore* generatori[MAX_COMPONENTI];
+    Generatore** generatori;
     int num_generatori;
 
-    Capacitor* capacitors[MAX_COMPONENTI];
-    int num_capacitors;
+    Capacitor** condensatori;
+    int num_condensatori;
 
-    Resonator* risonatori[MAX_COMPONENTI];
+    Resonator** risonatori;
     int num_risonatori;
 
-    Transmon* transmon[MAX_COMPONENTI];
+    Transmon** transmon;
     int num_transmon;
 
-    CoaxialCable* coassiali[MAX_COMPONENTI];
+    CoaxialCable** coassiali;
     int num_coassiali;
 } CircuitoElettronico;
 
@@ -42,5 +40,8 @@ void aggiungi_capacitor(CircuitoElettronico* c, Capacitor* cap);
 void aggiungi_risonatore(CircuitoElettronico* c, Resonator* r);
 void aggiungi_transmon(CircuitoElettronico* c, Transmon* t);
 void aggiungi_coassiale(CircuitoElettronico* c, CoaxialCable* cavo);
+
+void stampa_topologia(const CircuitoElettronico* c);
+void libera_circuito(CircuitoElettronico* c);
 
 #endif
